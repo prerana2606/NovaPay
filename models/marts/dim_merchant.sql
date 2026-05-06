5e9795c100f75f51f4ef3f734b779c87b428bf63
Prerana Mehta <preranamehta13@gmail.com>
2026-05-06 12:00:13 +0530

    Remove deleted files

--- models/marts/dim_merchant.sql
+++
@@ -1,3 +0,0 @@
-{{ config(materialized='table', schema='DIMENSIONAL') }}
-
-select * from {{ ref('stg_txn__merchants') }}
--- tests/assert_completed_payments_are_positive.sql
+++
@@ -1,7 +0,0 @@
-select
-    payment_id,
-    payment_amount,
-    payment_status
-from {{ ref('stg_txn__payments') }}
-where payment_status = 'completed'
-  and payment_amount < 0
